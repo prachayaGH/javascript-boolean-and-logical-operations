37f0698d5883d64f333c4b4e9ecfc4ebf02fd7db
unknown <Prachaya>
2025-02-24 14:57:02 +0700

    Boolean and Logical

--- ex-1.js
+++ ex-1.js
@@ -5,4 +5,7 @@ let isAllow;
 
 // Start coding here
 
+isOver18 = 18;
+hasCriminalBlacklist = true;
+isAllow = (isOver18 > 18) && !hasCriminalBlacklist
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -9,4 +9,10 @@ let isPlatinum;
 
 let hasPromotion;
 
+lastMonthPaidMoreThan4000 = 4001;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+hasPromotion = ((lastMonthPaidMoreThan4000 > 4000) && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || isPlatinum;
 console.log(hasPromotion);
